ba91f0cbc3d825b5d84f9f6535bea649f7fa07ce
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-20 14:09:30 +0100

    task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - use putchar
+* Return: 0 no errors
+*/
+int main(void)
+{
+	int number = '0';
+
+	while (number <= '9')
+	{
+		putchar(number);
+		if (number <= '8')
+			putchar(',');
+			putchar(' ');
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
