6650eae41d33479a8600447e02eb59593a327608
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-20 09:29:25 +0100

    second project

---
+++ hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print size of diff. var.
+ * Return: 1 must have an error
+ */
+int main(void)
+{
+	"\"and that piece of art is useful"\" - Dora Korpar, 2015-10-19"
+write()
+}
