c8ce0ebc39a7d720719ac3a79142e80bed3e9bd4
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-21 12:25:16 +0100

    err1 task2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,13 @@
 #include "main.h"
+#include "1-alphabet.c"
 /**
  * print_alphabet_x10 - use _putchar
  * Return: not needed
  */
 void print_alphabet_x10(void)
 {
-	char alphab = 'a';
 	int i = 0;
 
 	for (; i != 10 ; i++)
-	{
-		for (; alphab <= 'z'; alphab++)
-			_putchar (alphab);
-		_putchar ('\n');
-		alphab = 'a';
-	}
+		print_alphabet();
 }
