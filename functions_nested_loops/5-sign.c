440baceb6610a953af8f8dc40a1a44fe68c8b364
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-21 14:04:02 +0100

    Task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _print_sign - compare  0 + -
+ * @n: input value from main
+ * Return: return c value to main
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
