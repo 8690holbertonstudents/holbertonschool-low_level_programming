32796b2ef84f2a2156c0a9242d743e6c15457f33
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-21 21:09:41 +0100

    err1 Tast7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,23 @@
 #include "main.h"
+#include "6-abs.c"
 /**
 * print_last_digit - absolute value
-* @lastd: input parameter
+* @numb: input parameter
 * Return: return value to main
 */
-int print_last_digit(int lastd)
+int print_last_digit(int numb)
 {
-	lastd = lastd % 10;
+	int numbp, lastd;
+
+	if (numb < 0)
+	{
+		numbp = _abs(numb);
+		lastd = numbp % 10;
+	}
+	else
+	{
+		lastd = numb % 10;
+	}
+	_putchar ('0' + lastd);
 	return (lastd);
 }
