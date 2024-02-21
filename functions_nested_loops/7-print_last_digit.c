dbf2b93363d4cbcd5788ad5509ac9a32a561ddc1
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-21 15:30:29 +0100

    Task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_last_digit - absolute value
+* @lastd: input parameter
+* Return: return value to main
+*/
+int print_last_digit(int lastd)
+{
+	lastd = lastd % 10;
+	return (lastd);
+}
