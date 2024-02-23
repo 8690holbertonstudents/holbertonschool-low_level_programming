cea002caeb80e240df85764276a5736718290072
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-23 13:51:18 +0100

    Task 9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - print size of diff. var.
  * Return: 1 must have an error
  */
 int main(void)
 {
-	"\"and that piece of art is useful"\" - Dora Korpar, 2015-10-19"
-write()
+	char t1[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	return (write(1, &t1, 60));
 }
