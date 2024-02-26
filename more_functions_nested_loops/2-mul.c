65ecc2da07ed7f85f2236ab7a7f64efdd0a81cb5
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-26 09:52:40 +0100

    Task 3

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - just multiply
+ * @a: input value from main
+ * @b: input value from main
+ * Return: return c value to main
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
