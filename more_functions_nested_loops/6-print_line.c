16d02acbea206059c19f9aad98985b05d9cd8f16
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-26 13:23:39 +0100

    Task6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - print
+ * @n: input from main
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	for(i = 0 ; i < n ; i++)
+		if (n <= 0)
+		{
+		}
+		else
+		{
+			_putchar ('_');
+		}
+	_putchar ('\n');
+}
