6a7b675cc28513740a638a4e3a2421f9d140add7
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-26 15:24:57 +0100

    Task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - print square
+ * @size: input from main
+ *
+ */
+ void print_square(int size)
+{
+	int width, height;
+
+	for (height = 0 ; height < size ; height++)
+	{
+		for (width = 0 ; width < size ; width++)
+		{
+			_putchar ('#');
+		}
+	_putchar ('\n');
+	}
+}
