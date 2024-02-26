f44bf876c89e2e03766d0e662a528b877f9b0a45
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-26 15:51:52 +0100

    err2 Task8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -4,16 +4,24 @@
  * @size: input from main
  *
  */
- void print_square(int size)
+void print_square(int size)
 {
 	int width, height;
 
-	for (height = 0 ; height < size && size >=0 ; height++)
+	for (height = 0 ; height < size ; height++)
 	{
 		for (width = 0 ; width < size ; width++)
 		{
-			_putchar ('#');
+		if (size > 0)
+			{
+			 _putchar ('#');
+			}
+		else
+			{
+			_putchar ('\n');
+			}
 		}
 	_putchar ('\n');
 	}
 }
+
