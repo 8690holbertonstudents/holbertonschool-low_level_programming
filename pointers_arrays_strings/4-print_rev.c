44d7a4e3365036ef96f87499d6ff5faa3c649b40
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-27 15:39:37 +0100

    Task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - use pointer
+ * @s: input pointer from main
+ *
+ */
+void print_rev(char *s)
+{
+	int mypos;
+	int lasti = 0;
+
+	for (mypos = 0 ; s[mypos] != '\0'; mypos++)
+	{
+		lasti++;
+	}
+	while (lasti >= 0)
+	{
+		_putchar (s[lasti]);
+		lasti--;
+	}
+	_putchar ('\n');
+}
