b37fea476214dbf20c17b6c235a2018d810d1316
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-27 12:54:22 +0100

    Task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - use pointer
+ * @s: input pointer from main
+ * Return: len value to main
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0 ; s[len] != '\0' ; len++)
+	{
+	}
+	return (len);
+}
