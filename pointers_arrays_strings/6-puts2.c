bf043cd1d4b35c029e8810aeee2c692f8a45dde5
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-27 22:53:21 +0100

    Err1 Task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,7 @@ void puts2(char *str)
 	int i_pos;
 
 	for (i_pos = 0 ; str[i_pos] != '\0' ; i_pos++)
-		if ((str[i_pos] % 2) == 0)
+		if ((i_pos % 2) == 0)
 			_putchar (str[i_pos]);
 	_putchar ('\n');
 }
