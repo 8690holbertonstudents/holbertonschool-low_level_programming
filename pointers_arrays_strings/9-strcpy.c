db34bf15062dd1cea47fb5c84dd9ca4be8dfa0bd
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-28 13:45:56 +0100

    Err2 Task9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * *_strcpy - use pointer
  * @dest: input pointer from main
@@ -10,14 +9,14 @@ char *_strcpy(char *dest, char *src)
 {
 	int idx;
 
-	if (src != NULL)
-	{
 	for (idx = 0 ; src[idx] != '\0' ; idx++)
-		dest[idx] = src[idx];
-	}
-	else
-	{
-		dest = NULL;
-	}
+		if (src[0] != '\0')
+		{
+			dest[idx] = src[idx];
+		}
+		else
+		{
+			dest[0] = '\0';
+		}
 	return (dest);
 }
