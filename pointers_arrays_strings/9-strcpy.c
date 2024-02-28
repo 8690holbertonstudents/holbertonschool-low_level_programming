836b50db7a5abc3bb4ba14e76bb3a78a1bbe4a98
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-28 12:57:47 +0100

    Task9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_strcpy - use pointer
+ * @dest: input pointer from main
+ * @src: input pointer from main
+ * Return: Return pointer *dest to main
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int idx;
+
+	for (idx = 0 ; src[idx] != '\0' ; idx++)
+		dest[idx] = src[idx];
+	return (dest);
+}
