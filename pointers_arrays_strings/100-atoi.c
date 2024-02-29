6487fd6e2aa845a2af673abac66250c278c1ca0c
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-29 15:26:41 +0100

    Task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+ * _atoi - use pointer
+ * @s: input pointer from main
+ * Return: Return
+ */
+int _atoi(char *s)
+{
+	int i;
+	int ret = 0;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+        	ret = ret * 10 + s[i] - '0';
+		else if (s[i + 1] <= '0' && s[i] >= '9')
+		{
+			break;
+		}
+	}
+	return (ret);
+	ret = 0;
+}
