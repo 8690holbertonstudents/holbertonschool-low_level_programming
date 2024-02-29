92a08a950ba09461d62c86642262be9fa599521a
8690holbertonstudents <8690@holbertonstudents.com>
2024-02-29 11:14:39 +0100

    Don't understand...

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,20 +6,23 @@
  */
 void rev_string(char *s)
 {
-	char value;
-	int count;
-	int size1 = 0;
-	int size2 = 0;
+	char tmp;
+	int i, len, len1;
 
-	while (*s != '\0')
-		size1++;
+	len = 0;
+	len1 = 0;
 
-	size2 = size1 - 1;
-
-	for (count = 0; count< size1 / 2; count++)
+	while (s[len] != '\0')
 	{
-		value = s[count];
-		s[count] = s[size2];
-		s[size2 - 1] = value;
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
