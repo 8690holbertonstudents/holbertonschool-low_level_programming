a4baa2a71e54cf652ff34b5805b675f1481f7448
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-01 12:08:12 +0100

    Task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,23 @@
 #include "main.h"
 /**
- * *_strncat - use pointer
+ * *_strncpy - copy string to another
  * @src: source pointer
  * @dest: destination pointer
  * @n: n bytes at the end
  * Return: Contact pointers
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i_s = 0;
-	int i_d = 0;
-
-	while (dest[i_d] != '\0')
-	{
-		i_d++;
-	}
 
 	while (src[i_s] != '\0' && i_s < n)
 	{
-		dest[i_d++] = src[i_s];
+		dest[i_s] = src[i_s];
 		i_s++;
 	}
 
-	dest[i_d] = '\0';
+	for (; i_s < n ; i_s++)
+		dest[i_s] = '\0';
+
 	return (dest);
 }
