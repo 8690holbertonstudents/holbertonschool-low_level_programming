3615729648c77eea786a613ce1155832c173ac25
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-02 22:46:32 +0100

    Err2 Task6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,22 +6,22 @@
  */
 char *cap_string(char *s1)
 {
-	int i = 1;
-	int j = 0;
+	int i = 1, j = 0;
 
 	while (s1[i] != '\0')
 	{
-		if (s1[i - 1] == '.')
+		if (s1[0] >= 'a' && s1[0] <= 'z')
+			s1[0] = s1[0] - 32;
+		else if (s1[i - 1] == '.')
 			j = 1;
-		if (s1[i - 1] == 9)
+		else if (s1[i - 1] == 9)
 			j = 1;
-		if (s1[i - 1] == 10)
+		else if (s1[i - 1] == 10)
 			j = 1;
-		if (s1[i - 1] == ' ')
+		else if (s1[i - 1] == ' ')
 			j = 1;
 		if (s1[i] >= 'a' && s1[i] <= 'z' && j == 1)
 			s1[i] = s1[i] - 32;
-
 		j = 0;
 		i++;
 	}
