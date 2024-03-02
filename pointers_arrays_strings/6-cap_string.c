40a898211748338d1a55fe8f66dd916652b042f8
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-02 21:33:12 +0100

    Err Task6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,13 +6,23 @@
  */
 char *cap_string(char *s1)
 {
-	int i = 0;
+	int i = 1;
+	int j = 0;
 
 	while (s1[i] != '\0')
 	{
-		if (s1[i - 1] == ' ' && s1[i] >= 'a' && s1[i] <= 'z')
+		if (s1[i - 1] == '.')
+			j = 1;
+		if (s1[i - 1] == 9)
+			j = 1;
+		if (s1[i - 1] == 10)
+			j = 1;
+		if (s1[i - 1] == ' ')
+			j = 1;
+		if (s1[i] >= 'a' && s1[i] <= 'z' && j == 1)
 			s1[i] = s1[i] - 32;
 
+		j = 0;
 		i++;
 	}
 	return (s1);
