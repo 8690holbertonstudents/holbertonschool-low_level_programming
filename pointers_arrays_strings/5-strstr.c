20a09537a07e54d4bb157020e498b9c3c1e6e7a0
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-04 19:11:38 +0100

    Task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: input char pointer from main
+ * @needle: input char pointer from main
+ * Return: 0 no errors
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+			return (haystack);
+		haystack++;
+	}
+	return (0);
+}
