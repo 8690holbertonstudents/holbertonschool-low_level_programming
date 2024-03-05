ff1eda88a382f715426689a2a107bdceb392ead3
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-05 13:45:56 +0100

    Err 1 Task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _puts_recursion - function that prints a string
- * @a: input char pointer from main
+ * @s: input char pointer from main
  * Return: 0 no errors
  */
 void _puts_recursion(char *s)
@@ -9,7 +9,7 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 		_putchar (*s);
-		_puts_recursion (s + 1);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar ('\n');
