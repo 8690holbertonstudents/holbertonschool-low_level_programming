34cedf945b855b1545bf7b7487ae9fad9f609028
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-05 09:45:24 +0100

    Err 1 Task 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * _strstr - locates a substring
  * @haystack: input char pointer from main
@@ -7,13 +8,11 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-
-
 	while (*haystack != '\0')
 	{
 		if (*haystack == *needle)
 			return (haystack);
 		haystack++;
 	}
-	return (0);
+	return (NULL);
 }
