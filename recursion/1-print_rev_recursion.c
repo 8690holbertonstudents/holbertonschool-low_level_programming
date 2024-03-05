add9af08b52ecb65101e17c9eeeb3efc8d152944
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-05 14:24:24 +0100

    Task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function print a string in reverse
+ * @s: input char pointer from main
+ * Return: 0 no errors
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+	else
+		_putchar ('\n');
+}
