33fe18f329f263fc5dfffd3b548fbba75cb72d9d
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-06 09:15:45 +0100

    Err 1 Task 3

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _pow_recursion - return x raised to the power of y
+ * @x: input int num from main
+ * @y: input "power" num from main
+ * Return: return values to main
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	return (x ** y);
+}
