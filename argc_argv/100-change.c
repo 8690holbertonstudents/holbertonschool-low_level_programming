07903655767d82f15f0af9cdec0836c70d302038
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-08 21:23:25 +0100

    Upload Task 5

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -9,33 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int num = atoi(argv[1]);
 	int result = 0;
 
-	if (!(argc == 2))
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else if (num <= 0)
-		printf("0\n");
-	else
-	{
+		if (argc == 2)
+		{
+		int num = atoi(argv[1]);
 		while (num > 0)
 		{
-		if (num >= 25)
-			num -= 25;
-		else if (num >= 10)
-			num -= 10;
-		else if (num >= 5)
-			num -= 5;
-		else if (num >= 2)
-			num -= 2;
-		else if (num >= 1)
-			num -= 1;
-		result++;
+			result = num / 25;
+			num %= 25;
+			result += num / 10;
+			num %= 10;
+			result += num / 5;
+			num %= 5;
+			result += num / 2;
+			num %= 2;
+			result += num;
+			num--;
 		}
 		printf("%d\n", result);
-	}
+		}
+		if (argc == 1 || argc > 2)
+		{
+			printf("Error\n");
+			return (1);
+		}
 	return (0);
 }
