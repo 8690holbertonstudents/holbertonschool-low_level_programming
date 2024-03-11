0daa43b9a3eadc28c95771be42c80aec4d72cea9
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-11 14:12:42 +0100

    Err 1 - Task 0 - P13/30

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,7 +12,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	ptr = malloc(sizeof(c) * size);
-	for (i = 0 ; i < size ; i++)
-		ptr[i] = c;
+
+	if (size == 0)
+		return (NULL);
+	else
+	{
+		for (i = 0 ; i < size ; i++)
+			ptr[i] = c;
+	}
 	return (ptr);
 }
