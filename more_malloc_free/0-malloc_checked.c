656800763a9dc20b476860d9309f54c114258bcb
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-12 15:57:04 +0100

    Task 0 - P14/30

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: input size from main
+ * Return: return pointer to main
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
