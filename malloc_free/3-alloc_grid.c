568f8f69402d85d18c85c42b5e184be6a5b76905
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-12 11:14:48 +0100

    Err 2 - Task 3 - P13/30

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@
 int **alloc_grid(int width, int height)
 {
 	int **ptr;
-	int i; /*r, c;*/
+	int i;
 
 	ptr = malloc(width * sizeof(int *));
 	if (!(ptr == NULL))
@@ -22,14 +22,8 @@ int **alloc_grid(int width, int height)
 			if (ptr[i] == NULL)
 				return (NULL);
 		}
-		/*for (r = 0 ; r < height ; r++)
-		{
-			for (c = 0 ; c < width ; c++)
-				ptr[c][r] = 0;
-		}*/
 	return (ptr);
 	}
 	else
 		return (NULL);
-	return (0);
 }
