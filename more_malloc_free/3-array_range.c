2819df2a4865b365048b7e241dfc1e739bafa53e
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-13 15:35:14 +0100

    Err 4 - Task 3 - P14/30

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc((size + 1) * sizeof(int *));
+	ptr = malloc((size + 1) * sizeof(int));
 	if (!(ptr == NULL))
 	{
 		for (i = 0 ; i <= max ; i++)
