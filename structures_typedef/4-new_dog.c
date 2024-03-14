fb3a63732cf93467f221c7137f774842d9e3ce45
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-14 18:18:58 +0100

    Task 4 - P15/30

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * new_dog - function that creates a new dog
+ * @name: name element of structure dog
+ * @age: age element of structure dog
+ * @owner: owner element of structure dog
+ * Return: Pointer to main
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *p_dog;
+
+	p_dog = malloc(sizeof(struct dog));
+	if (!(p_dog == 0))
+	{
+		p_dog->name = name;
+		p_dog->age = age;
+		p_dog->owner = owner;
+	}
+	else
+		return (NULL);
+	return (p_dog);
+}
