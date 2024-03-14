994485799e55ea1bb0e1f10a45f8e2e6f5c3d1f2
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-14 15:33:19 +0100

    Upload task 2 - P14/30

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,18 +9,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr;
+	char *ptr;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc((nmemb * size) * 8);
+	ptr = malloc(nmemb * size);
 
 	if (!(ptr == NULL))
 	{
 		for (i = 0 ; i < nmemb ; i++)
-			ptr[i] = 0;
+			ptr[i] = '\0';
 	}
 	else
 		return (NULL);
