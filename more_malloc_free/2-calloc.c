209cbf6f83cb698b3bf1ca7c334f2f3cf10579be
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-14 15:38:38 +0100

    999 Task 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
@@ -19,8 +19,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (!(ptr == NULL))
 	{
-		for (i = 0 ; i < nmemb ; i++)
-			*((char *)ptr + 1) = 0;
+		for (i = 0 ; i < (nmemb * size) ; i++)
+			ptr[i] = '\0';
 	}
 	else
 		return (NULL);
