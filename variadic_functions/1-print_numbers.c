aee29b24ac1b8eb703b8c0da351d41da286713d4
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-20 09:50:07 +0100

    C - Variadic functions - T1 - Err2

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,7 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	if (n == 0)
+	{
+		printf("\n");
 		exit(1);
+	}
 
 	va_start(args, n);
 	for (i = 0 ; i < n ; i++)
