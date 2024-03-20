c2bc5c8eb98ef51be74643171874eb0448d600bd
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-20 09:38:10 +0100

    C - Variadic functions - T1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - function that returns
+ * the sum of all its parameters
+ * @separator: numbers to sum together
+ * @n: numbers to sum together
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	if (n == 0)
+		exit(0);
+
+	va_start(args, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		if (separator == NULL || (i + 1) == n)
+			printf("%d",va_arg(args, int));
+		else
+			printf("%d%s",va_arg(args, int), separator);
+	}
+	printf("\n");
+	va_end(args);
+}
