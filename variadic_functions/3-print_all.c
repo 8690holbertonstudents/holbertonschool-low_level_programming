6efe8d553bfc0fa864c45db1a5771fdc1bc1e6f9
8690holbertonstudents <8690@holbertonstudents.com>
2024-03-22 09:22:18 +0100

    C - Variadic functions - T3 - Err8

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,7 +7,6 @@
 void print_all(const char * const format, ...)
 {
 	int i = 0, state = 0;
-	char *string;
 	va_list args;
 
 	if (format == NULL)
@@ -16,35 +15,50 @@ void print_all(const char * const format, ...)
 		exit(0);
 	}
 	va_start(args, format);
+
 	while (format[i] != '\0')
 	{
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(args, int));
-			state = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(args, int));
-			state = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(args, double));
-			state = 1;
-			break;
-		case 's':
-			string = va_arg(args, char *);
-			printf("%s", string ? string : "(nil)");
-			state = 1;
-			break;
-		default:
-			state = 0;
-			break;
-		}
+		select_case(format[i], args, state);
 		i++;
 		if (state == 1 && format[i] != '\0')
 			printf(", ");
 	}
 	printf("\n");
 	va_end(args);
+}
+/**
+ * select_case - print output of "case" format
+ * @comp: char to switch case
+ * @args: va_list args from print_all variadic function
+ * @bool: status value from print_all (true or false)
+ * Return: bool to print_all
+ */
+int select_case(char comp, va_list args, int bool)
+{
+	char *string;
+
+	switch (comp)
+		{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			bool = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			bool = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			bool = 1;
+			break;
+		case 's':
+			string = va_arg(args, char *);
+			printf("%s", string ? string : "(nil)");
+			bool = 1;
+			break;
+		default:
+			bool = 0;
+			break;
+		}
+	return (bool);
 }
