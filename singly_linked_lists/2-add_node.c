370badc88dd590b1eb6f2ff0631fec410e65b6fd
8690holbertonstudents <8690@holbertonstudents.com>
2024-04-02 14:55:01 +0200

    P16/30 - Try correction to task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,10 +13,12 @@ list_t *add_node(list_t **head, const char *str)
 	newnode = malloc(sizeof(list_t));
 	if (newnode == NULL)
 		return (NULL);
-	newnode->len = strlen(str);
-	newnode->str = strdup(str);
-	newnode->next = *head;
-	*head = newnode;
-
-	return (*head);
+	else
+	{
+		newnode->len = strlen(str);
+		newnode->str = strdup(str);
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
 }
