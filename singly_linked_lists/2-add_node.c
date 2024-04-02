e7ed62fcfc247e720d174ece23129ab46395b10a
8690holbertonstudents <8690@holbertonstudents.com>
2024-04-02 17:24:53 +0200

    P16/30 - Correction to bad output

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,11 +15,17 @@ list_t *add_node(list_t **head, const char *str)
 	if (newnode == NULL)
 		return (NULL);
 
+	newnode->str = strdup(str);
+	if (newnode->str == NULL)
+	{
+		free(newnode);
+		return (NULL);
+	}
+
 	while (str[i] != '\0')
 		i++;
 
 	newnode->len = i;
-	newnode->str = strdup(str);
 	newnode->next = *head;
 	*head = newnode;
 
