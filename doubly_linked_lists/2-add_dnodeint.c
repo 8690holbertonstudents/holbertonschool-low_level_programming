db40b57ce5a9e56da6e2f8c5cd84a4dd5b50db78
8690holbertonstudents <8690@holbertonstudents.com>
2024-04-03 14:30:59 +0200

    Double LL - Task 2 correction for NULL

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,6 +15,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (newnode == NULL)
 		return (NULL);
 
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (*head);
+	}
+
 	newnode->prev = NULL;
 	newnode->n = n;
 	newnode->next = NULL;
