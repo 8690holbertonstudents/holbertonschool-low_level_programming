336f15e2e8c48d0eb5925aadf15e9d15053dbc6d
8690holbertonstudents <8690@holbertonstudents.com>
2024-04-04 15:15:49 +0200

    Double LL - T7 - 3err

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	newnode->n = n;
-	newnode->next = NULL;
+	/*newnode->next = NULL;*/
 
 	if (*h == NULL)
 	{
