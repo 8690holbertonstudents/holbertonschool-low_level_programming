72b689f6d4f6bda68b1867533f21a9eaed29615d
8690holbertonstudents <8690@holbertonstudents.com>
2024-04-05 11:52:49 +0200

    Bit manipulation - T1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <limits.h>
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: input number from main
+ * Return: The number in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int flag = 0;
+	unsigned long mask =  1UL << (sizeof(unsigned long int) * 8 - 1);
+
+	if (n == 0)
+		_putchar('0');
+
+	while (mask > 0)
+	{
+		if (n & mask)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag == 1)
+			_putchar('0');
+
+		mask = mask >> 1;
+	}
+}
