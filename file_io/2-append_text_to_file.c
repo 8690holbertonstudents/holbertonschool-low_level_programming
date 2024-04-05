38ebfaf4d2b154ffed28290a485cf60c43c7cae5
8690holbertonstudents <8690@holbertonstudents.com>
2024-04-06 01:45:03 +0200

    File I/O - T2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: pointer to filename from main
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t i = 0;
+	char buffer[2048];
+	ssize_t b_write;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (0);
+
+	if (text_content == NULL)
+		return (-1);
+
+	while (text_content[i] != '\0')
+		i++;
+
+	b_write = write(fd, buffer, i);
+
+	if (b_write == -1)
+		return (0);
+
+	close(fd);
+	return (1);
+}
+
